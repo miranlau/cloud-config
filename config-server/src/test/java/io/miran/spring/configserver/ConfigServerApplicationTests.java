8786fb36abc6ab8cc3d873b61026a092698d8888
LMY <mingyuan.liu@tieto.com>
2019-05-05 11:24:33 +0800

    init config-server module

---
+++ config-server/src/test/java/io/miran/spring/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,18 @@
+package io.miran.spring.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("integrationTest")
+public class ConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
